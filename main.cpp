eae570e1beae9a3f78914368c65c52d0ebf2946e
mahliyo <m.yoldashova@newuu.uz>
2025-01-16 14:55:36 +0500

    m

---
+++ main.cpp
@@ -0,0 +1,16 @@
+  #include <iostream>
+  using namespace std;
+  int main() {
+           cout << "Hello World!" << endl;
+           cout<<"Bye"<<endl;
+           int age;
+           cout<<"Enter your age: ";
+           cin>>age;
+           cout<<"Your age is: "<<age<<endl;
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
